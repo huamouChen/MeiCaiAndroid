13bc77bf9e737eb41a5f005fbccb966415e11272
风华正茂回谋一笑 <chenhm4444@gmail.com>
2017-09-29 16:48:32 +0800

    “我的”、“购物车”界面搭建

--- MeiCai/app/src/main/java/com/example/chenhuamou/meicai/MainActivity.java
+++ MeiCai/app/src/main/java/com/example/chenhuamou/meicai/MainActivity.java
@@ -162,7 +162,7 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
     public void onPageScrollStateChanged(int state) {
 
     }
-    
+
     /*
     * viewPager Adapter
     * */
---
+++ MeiCai/app/src/main/java/com/example/chenhuamou/meicai/util/RoundImageView.java
@@ -0,0 +1,83 @@
+package com.example.chenhuamou.meicai.util;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+
+/**
+ * Created by chenhuamou on 2017/9/29.
+ */
+
+public class RoundImageView extends android.support.v7.widget.AppCompatImageView {
+
+    private Paint mPaint;
+
+    public RoundImageView(Context context) {
+        this(context, null);
+    }
+
+    public RoundImageView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public RoundImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mPaint = new Paint();
+    }
+
+
+
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Drawable drawable = getDrawable();
+        if (null != drawable) {
+            Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+            Bitmap b = getCircleBitmap(bitmap, 14);
+            final Rect rectSrc = new Rect(0, 0, b.getWidth(), b.getHeight());
+            final Rect rectDest = new Rect(0,0,getWidth(),getHeight());
+            mPaint.reset();
+            canvas.drawBitmap(b, rectSrc, rectDest, mPaint);
+
+        } else {
+            super.onDraw(canvas);
+        }
+    }
+
+    /**
+     * 获取圆形图片方法
+     * @param bitmap
+     * @param pixels
+     * @return Bitmap
+     * @author caizhiming
+     */
+    private Bitmap getCircleBitmap(Bitmap bitmap, int pixels) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        mPaint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        mPaint.setColor(color);
+        int x = bitmap.getWidth();
+
+        canvas.drawCircle(x / 2, x / 2, x / 2, mPaint);
+        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, mPaint);
+        return output;
+
+
+    }
+}
